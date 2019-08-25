5bc377638f75ffaa10296592358f4b84d449396f
ZakkoYan <50697490+ZakkoYan@users.noreply.github.com>
2019-08-25 17:32:04 +0800

    Initial commit

---
+++ src/com/yzk/ArrayList_Test/Test01.java
@@ -0,0 +1,15 @@
+package com.yzk.ArrayList_Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test01 {
+    public static void main(String args[])
+    {
+        List<String> arrayList=new ArrayList<>();
+        arrayList.add("aaa");
+        arrayList.add("bbb");
+        arrayList.add("ccc");
+        arrayList.add("ddd");
+    }
+}
