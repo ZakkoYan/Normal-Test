39acc6d26e784dca18b173bb66788ae8025bdbcb
ZakkoYan <50697490+ZakkoYan@users.noreply.github.com>
2019-08-25 18:49:44 +0800

    场景切换更新

--- src/com/yzk/ArrayList_Test/Test01.java
+++ src/com/yzk/ArrayList_Test/Test01.java
@@ -1,6 +1,7 @@
 package com.yzk.ArrayList_Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Test01 {
@@ -11,5 +12,20 @@ public class Test01 {
         arrayList.add("bbb");
         arrayList.add("ccc");
         arrayList.add("ddd");
+        //普通的for循环遍历
+        for(int i=0;i<arrayList.size();i++)
+        {
+            System.out.println(arrayList.get(i));
+        }
+        //使用Iterator遍历ArrayList
+        Iterator<String> iter=arrayList.iterator();
+        while(iter.hasNext())
+            System.out.println(iter.next()+"");
+
+        //使用增强for循环
+        for(String s:arrayList)
+            System.out.println(s);
     }
 }
+
+
