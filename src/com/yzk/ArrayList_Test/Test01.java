0e65d3be16e00cda4c1a77a8183e3d1aea716070
ZakkoYan <867190347@qq.com>
2019-08-25 22:24:31 +0800

    。

--- src/com/yzk/ArrayList_Test/Test01.java
+++ src/com/yzk/ArrayList_Test/Test01.java
@@ -1,31 +1,29 @@
 package com.yzk.ArrayList_Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.Iterator;
+        import java.util.List;
 
 public class Test01 {
-    public static void main(String args[])
-    {
-        List<String> arrayList=new ArrayList<>();
+    public static void main(String args[]) {
+        List<String> arrayList = new ArrayList<>();
         arrayList.add("aaa");
         arrayList.add("bbb");
         arrayList.add("ccc");
         arrayList.add("ddd");
         //普通的for循环遍历
-        for(int i=0;i<arrayList.size();i++)
-        {
+        for (int i = 0; i < arrayList.size(); i++) {
             System.out.println(arrayList.get(i));
         }
         //使用Iterator遍历ArrayList
-        Iterator<String> iter=arrayList.iterator();
-        while(iter.hasNext())
-            System.out.println(iter.next()+"");
-
+        Iterator<String> iter = arrayList.iterator();
+        while (iter.hasNext())
+            System.out.println(iter.next());
         //使用增强for循环
-        for(String s:arrayList)
+        for (String s : arrayList)
             System.out.println(s);
     }
 }
+
 
 
